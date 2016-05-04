d0e296d45f083d37d8962ae855e441a48587a7d3
gatapov <vgatapov@gmail.com>
2016-05-04 22:29:21 +0800

    ajax send post

--- lib/send.php
+++ lib/send.php
@@ -2,27 +2,31 @@
 <?php 
 require 'config.php';
  
- 
 // Принимаем данные с формы 
  $phone=$_POST['phone']; 
-
  // Отправляем письмо админу  
 mail($adminemail, 'сообщение', $phone); 
 
 $body=file_get_contents("http://sms.ru/sms/send?api_id=".urlencode(iconv("windows-1251","utf-8","$key"))."&to=".urlencode(iconv("windows-1251","utf-8","$num"))."&text=".urlencode(iconv("windows-1251","utf-8","$phone")));
 
-
-
 // Выводим сообщение пользователю 
+print "<script language='Javascript'>
+$('.formheader').css({'visibility': 'hidden'});
+</script>";
+echo "<h1 id='result'>";
+print "Спасибо за заявку!";
+echo "</h1>";
+echo "<p id='result'>";
+print "мы свяжемся с вами в ближайшее время";
+echo "</p>";
+print "<script language='Javascript'>
+$('.resultsbtn').click(function (){
+$('.popup, .overlay').css({'opacity': 0, 'visibility': 'hidden'});
+$('.callmeicon').css({'visibility': 'visible'});
+});
+</script>
+<div class='resultsbtn'>OK</div>";
 
-print "<script language='Javascript'><!-- 
-function reload() {location = \"$backurl\"}; setTimeout('reload()', 2000); 
-//--></script> 
- 
-$msg 
- 
-<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
 
 exit; 
- 
-?>
+ ?>
--- tpl/form.php
+++ tpl/form.php
@@ -2,13 +2,19 @@
 require '../lib/config.php';
 ?>
 <head>
-<link rel="stylesheet" href="/callme/tpl/css/style.css">
+<link rel="stylesheet" href="/callme/tpl/css/callmestyle.css">
+<link rel="stylesheet" href="/callme/tpl/css/css/callmebootstrap.css">
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <body>
 <div class="container">
-		<div class="callmeicon" style="position:fixed; right: 30px; bottom: 30px; min-width:30px; max-width:80px; height=auto;">
-		<img src="./callme/tpl/img/phone.png" class="img-responsive" id="callmeimg" width="80px" height="80px"/>
+
+		<div class="callmeicon" style="position:fixed; right: 30px; bottom: 30px;">
+		<div class="callme_phone">
+		<div class="callme_wrapper">
+		<span class="glyphicon glyphicon-earphone"></span>
+		</div>
+		</div>
 		</div>
 </div>
 
@@ -17,46 +23,58 @@ require '../lib/config.php';
 <div class="popup">
 <div class="close_window">ЗАКРЫТЬ Х</div>
 
-<form id="modalform" action= "/callme/lib/send.php" method= "POST"> 
+<form id="modalform" action="javascript:void(null);" onsubmit="call()"> 
+ <div class="formheader">
  <div class="formtitle"><?=$callmetext?></div>
+
 <input id="modalform" type= "text" name= "phone" placeholder="ваш телефон" required/>
  
 <input id="modalformbtn" type= "submit" value= "<?=$btntext?>"> 
-
+</div>
  <div class="formbottom"><?=$bottomtext?><br><?=$bottomphone?></div>
   
 </form>
-
+ <div class="results">
+ </div>
 <div class="copyright">© <a href="http://gatapov.com/2015/12/02/скрипт-обратного-звонка-с-смс-уведомл/" target="blank" style="color: #B5B4B4; text-decoration: none;">Callme script</a></div>
 </div>
 
 <script type="text/javascript">
-$('#callmeicon, .callmeicon, .close_window, .overlay').click(function (){
+$('#callmeicon, .resultsbtn, .callmeicon, .close_window, .overlay').click(function (){
 $('.popup, .overlay').css({'opacity': 0, 'visibility': 'hidden'});
+$('.callmeicon').css({'visibility': 'visible'});
 });
 $('#callmeicon, .callmeicon').click(function (e){
 $('.popup, .overlay').css({'opacity': 1, 'visibility': 'visible'});
+$('.callmeicon').css({'visibility': 'hidden'});
 e.preventDefault();
 });
+/*
+var timerId = setInterval(function() {
+  $(".callme_wrapper").css("transform", "rotate(360deg)");
+
+}, 2000);
+*/
+
+function call() {
+ 	var msg = $('#modalform').serialize();
+        $.ajax({
+          type: 'POST',
+          url: 'callme/lib/send.php',
+          data: msg,
+          success: function(data) {
+            $('.results').html(data);
+          },
+          error:  function(xhr, str){
+	    alert('Возникла ошибка: ' + xhr.responseCode);
+        }
+    });
+ 
+}
+
+	
+	
 </script>
 
-
-<script type="text/javascript">
-
-$(function() {
-    $("img#callmeimg")
-        .mouseover(function() { 
-            var src = $(this).attr("src").match(/[^\.]+/) + "_over.png";
-            $(this).attr("src", src);
-        })
-        .mouseout(function() {
-            var src = $(this).attr("src").replace("_over.png", ".png");
-            $(this).attr("src", src);
-        });
-});
-</script>
-<script>
-setInterval(function() {$( ".callmeicon" ).effect( "shake" );}, 5000);
-</script>
 
 </body>
